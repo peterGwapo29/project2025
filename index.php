030a62563bde40a1351c8e4840dd4b83aa4beca9
peterGwapo29 <peterolanolan@gmail.com>
2025-04-21 21:35:44 +0800

    ifelse statement

--- index.php
+++ index.php
@@ -6,8 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>This is another branch</h1>
-    <h1>push this project</h1>
-    <h1>another text to push</h1>
+
+    <form action="index.php" method="post">
+
+    <label>Age:</label><br>
+    <input type="text" name="age" placeholder="Enter your age"><br>
+    
+    <button type="submit">SUBMIT</button>
+    
+    </form>
+
 </body>
 </html>
+
+<?php
+
+    $age = $_POST['age'];
+
+    if($age < 18) {
+        echo "You're an underage";
+    }elseif($age >= 18 && $age < 60) {
+        echo "You're an adult";
+    }else{
+        echo "You're a senior citizen";
+    }
+
+
+?>
