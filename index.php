1329fb46ae8e1716ec0153c05ec6b860f933885e
peterGwapo29 <peterolanolan@gmail.com>
2025-04-27 15:59:29 +0800

    advance practice1

--- index.php
+++ index.php
@@ -7,26 +7,39 @@
 </head>
 <body>
 
-    <form action="index.php" method="post">
-
-    <label>DAY:</label><br>
-    <input type="text" name="day" placeholder="Enter Day"><br>
-    
-    <button type="submit">SUBMIT</button>
-    
-    </form>
+   <form action="index.php" method="post">
+    <input type="radio" name="credit_card" value="Visa1">Visa1
+        <br>
+        <input type="radio" name="credit_card" value="Visa2">Visa2
+        <br>
+        <input type="radio" name="credit_card" value="Visa3">Visa3
+        <br>
+        <input type="submit" name="confirm" value="confirm">
+   </form>
 
 </body>
 </html>
 
 <?php
+    $selected_card = null;
 
-    $days = $_POST['day'];
-
-    while($i < 5){
-        echo "<br>Day: $days <br>";
-        $i++;
+    if(isset($_POST["confirm"])) {
+        if(isset($_POST["credit_card"])){
+            $selected_card = $_POST['credit_card'];
+        }
+        switch ($selected_card) {
+            case 'Visa1':
+                echo "You selected Visa1";
+                break;
+            case 'Visa2':
+                echo "You selected Visa2";
+                break;
+            case 'Visa3':
+                echo "You selected Visa3";
+                break;
+            default:
+                echo "No card selected";
+        }
     }
-
 
 ?>
