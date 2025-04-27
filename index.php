aeb3b5dd0b34e48f03728df95f5278bb4e586457
peterGwapo29 <peterolanolan@gmail.com>
2025-04-27 21:49:26 +0800

    dbmysql searching

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 
 <form action="index.php" method="post">
     <label for="">Username:</label><br>
-    <input type="text" name="user" placeholder="Enter your username" required> <br>
+    <input type="text" name="user" placeholder="Search your username" required> <br>
     
-    <label for="">Password:</label><br>
-    <input type="password" name="password" placeholder="Enter your password" required> <br>
+    <!-- <label for="">Password:</label><br>
+    <input type="password" name="password" placeholder="Enter your password" required> <br> -->
     <br>
     <input type="submit" name="confirm" value="confirm">
 </form>
@@ -24,16 +24,28 @@
 <?php
     include("database.php");
 
-    $username = $_POST["user"];
-    $password = $_POST["password"];
-    $sql = null;
+    if (isset($_POST["confirm"])) {
+        try {
+            $username = mysqli_real_escape_string($conn, $_POST["user"]);
+            $sql = "SELECT * FROM employee_account WHERE user = '$username'";
+            $result = mysqli_query($conn, $sql);
 
-    if(isset($username)){
-        $sql = "INSERT INTO employee_account (user, password)
-                VALUES ('$username', '$password')";
-        mysqli_query($conn, $sql);
-    }else{
-        echo "Invalid to create";
+            if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+
+                echo "<h1>Employee Account:</h1>";
+
+                echo "<h3>ID: " . $row["id"] . "</h3>";
+                echo "<h3>USERNAME: " . $row["user"] . "</h3>";
+                echo "<h3>PASSWORD: " . $row["password"] . "</h3><br>";
+            } else {
+                echo "No user found!";
+            }
+
+            mysqli_close($conn);
+
+        } catch (mysqli_sql_exception $e) {
+            echo "Failed to search!<br>";
+        }
     }
-
 ?>
