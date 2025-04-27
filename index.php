84b93c8c62fb7a42fc61ab673968e9c70cfce0e4
peterGwapo29 <peterolanolan@gmail.com>
2025-04-27 21:14:57 +0800

    db sql inserting

--- index.php
+++ index.php
@@ -7,39 +7,33 @@
 </head>
 <body>
 
-   <form action="index.php" method="post">
-    <input type="radio" name="credit_card" value="Visa1">Visa1
-        <br>
-        <input type="radio" name="credit_card" value="Visa2">Visa2
-        <br>
-        <input type="radio" name="credit_card" value="Visa3">Visa3
-        <br>
-        <input type="submit" name="confirm" value="confirm">
-   </form>
-
+<form action="index.php" method="post">
+    <label for="">Username:</label><br>
+    <input type="text" name="user" placeholder="Enter your username" required> <br>
+    
+    <label for="">Password:</label><br>
+    <input type="password" name="password" placeholder="Enter your password" required> <br>
+    <br>
+    <input type="submit" name="confirm" value="confirm">
+</form>
+    
 </body>
 </html>
 
-<?php
-    $selected_card = null;
 
-    if(isset($_POST["confirm"])) {
-        if(isset($_POST["credit_card"])){
-            $selected_card = $_POST['credit_card'];
-        }
-        switch ($selected_card) {
-            case 'Visa1':
-                echo "You selected Visa1";
-                break;
-            case 'Visa2':
-                echo "You selected Visa2";
-                break;
-            case 'Visa3':
-                echo "You selected Visa3";
-                break;
-            default:
-                echo "No card selected";
-        }
+<?php
+    include("database.php");
+
+    $username = $_POST["user"];
+    $password = $_POST["password"];
+    $sql = null;
+
+    if(isset($username)){
+        $sql = "INSERT INTO employee_account (user, password)
+                VALUES ('$username', '$password')";
+        mysqli_query($conn, $sql);
+    }else{
+        echo "Invalid to create";
     }
 
 ?>
